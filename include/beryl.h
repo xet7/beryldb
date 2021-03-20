4042a954b799b6a6fb65eeaba858d57cb22b9789
Carlos Ferry <carlos.ferry.sauterel@gmail.com>
2021-03-20 10:45:33 -0300

    Beryl's main include.

--- include/beryl.h
+++ include/beryl.h
@@ -1,2 +1,22 @@
 
 #pragma once
+
+extern Beryl* Nucleus;
+
+class Beryl
+{
+    public:
+
+        /*
+         * Await time between loop intervals. This variable
+         * is used to avoid bottlenecks.
+         */
+
+        unsigned int await_time;
+
+        /* If set to true, then our await_time var cannot be changed. */
+
+        bool Locked;
+        
+        
+};
