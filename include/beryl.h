00460de32ad3ad57fce98764febc9b15b5166971
Carlos Ferry <carlos.ferry.sauterel@gmail.com>
2021-05-04 16:47:04 -0400

    Beryl class.
    Main class, which serve has an initializer to other classes.

---
+++ include/beryl.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <csignal>
+#include <map>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "constants.h"
+#include "ilist.h"
+#include "flat_map.h"
+#include "adaptables.h"
+#include "aligned_storage.h"
+#include "forwards.h"
+#include "typedefs.h"
+#include "bmaps.h"
+#include "converter.h"
+#include "stdhelpers.h"
+
