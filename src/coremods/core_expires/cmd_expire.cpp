cfad2f0ba7684c84dad544d9b1e4d9783c8dc95e
Carlos Ferry <cferry@beryldb.com>
2021-07-18 18:32:50 -0400

    Small changes.

--- src/coremods/core_expires/cmd_expire.cpp
+++ src/coremods/core_expires/cmd_expire.cpp
@@ -239,3 +239,76 @@ COMMAND_RESULT CommandDBEReset::Handle(User* user, const Params& parameters)
           Kernel->Store->Expires->DatabaseReset(database->GetName());
           return SUCCESS;
 }
+
+CommandSetex::CommandSetex(Module* Creator) : Command(Creator, "SETEX", 3, 3)
+{
+          group = 'e';
+          syntax = "<seconds> <key> <value>";
+}
+
+COMMAND_RESULT CommandSetex::Handle(User* user, const Params& parameters) 
+{       
+          const std::string& seconds = parameters[0];
+          const std::string& key = parameters[1];
+          const std::string& value = parameters[2];
+
+          if (!CheckKey(user, key))
+          {
+              return FAILED;
+          }
+
+          /* Similar to a set, setex must have a proper inserting format. */
+          
+          if (!CheckFormat(user, value))
+          {
+               return FAILED;
+          } 
+
+          if (!CheckValidPos(user, seconds))
+          {
+               return FAILED;
+          }
+
+          /* We convert expiring time to int. */
+          
+          ExpireHelper::Setex(user, convto_num<unsigned int>(seconds), key, value);
+          return SUCCESS;
+}
+
+CommandExpireAT::CommandExpireAT(Module* Creator) : Command(Creator, "EXPIREAT", 2, 2)
+{
+          group = 'e';
+          syntax = "<key> <epoch time>";
+}
+
+COMMAND_RESULT CommandExpireAT::Handle(User* user, const Params& parameters) 
+{    
+          const std::string& key = parameters[0];
+          const std::string& seconds = parameters[1];
+
+          if (!CheckKey(user, key))
+          {
+               return FAILED;
+          }
+          
+          if (!CheckValid(user, seconds))
+          {
+               return FAILED;
+          }
+
+          if (!CheckValidPos(user, seconds))
+          {
+              return FAILED;
+          }
+                  
+          unsigned int exp_usig = convto_num<unsigned int>(seconds);
+
+          if ((time_t)exp_usig < Kernel->Now())
+          {
+                 user->SendProtocol(ERR_INPUT, PROCESS_ERROR);
+                 return FAILED;
+          }
+          
+          ExpireHelper::ExpireAT(user, key, exp_usig);
+          return SUCCESS;
+}
