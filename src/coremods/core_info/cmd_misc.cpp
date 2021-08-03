fcf5709dbadbc0c8db142dac7e5c632f40c19645
Carlos Ferry <cferry@beryldb.com>
2021-08-02 22:01:49 -0400

    Minor changes.

--- src/coremods/core_info/cmd_misc.cpp
+++ src/coremods/core_info/cmd_misc.cpp
@@ -13,12 +13,6 @@
 
 #include "beryl.h"
 #include "core_info.h"
-#include "managers/keys.h"
-#include "engine.h"
-
-#include "managers/user.h"
-#include "managers/databases.h"
-#include "brldb/dbmanager.h"
 
 CommandTime::CommandTime(Module* parent) : ServerTargetCommand(parent, "TIME")
 {
